085be13ac06858bcb51ded0188b1ffe4babb49c2
josue <jvl_josue@hotmail.com>
2019-10-15 00:37:05 -0500

    Front con MVC, ASP Core

---
+++ Banco.Web/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Banco.Web.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Banco.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            Movie movie = new Movie()
+            {
+
+                Title = "El bromas",
+                RelaseDate = new DateTime(1997, 10, 28)
+            };
+            
+            return View(movie);
+            //return Content("Helo Work!!");
+        }
+    }
+}
---
+++ Banco.Web/Models/Movie.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Banco.Web.Models
+{
+    public class Movie
+    {
+        /// <summary>
+        /// Titulo de la Pelicula
+        /// </summary>
+        public string Title { get; set; }
+
+        /// <summary>
+        /// Dia del Estreño
+        /// </summary>
+        public DateTime RelaseDate { get; set; } 
+    }
+}
